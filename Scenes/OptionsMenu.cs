129cbf1daf041c4b61e3efa717bc6706cc8f5d7b
axell.tielemans <axelltielemans@gmail.com>
2025-02-10 20:56:10 +0100

    optionsmenu actions

--- Scenes/OptionsMenu.cs
+++ Scenes/OptionsMenu.cs
@@ -1,6 +1,27 @@
-using Godot;
 using System;
+using Godot;
+using Godot.Collections;
+
 
 public partial class OptionsMenu : Container
 {
+
+
+    public Action<BaseButton> ButtonFocused;
+    public Action<BaseButton> ButtonPressed;
+    
+
+
+    public Array<Node>  getButtons(){
+        return GetChildren();
+    }
+
+    public void connectToButton(){
+
+        foreach(Button button in getButtons()){
+            button.FocusEntered += () => ButtonFocused?.Invoke(button);
+            button.Pressed += () => ButtonPressed?.Invoke(button);
+        }
+
+    }
 }
